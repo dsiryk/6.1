79cac7f445b1fda810486b10a67117ec82816d7c
dezag <98271110+dsiryk@users.noreply.github.com>
2025-03-20 16:20:59 +0200

    Update Program.cs

--- 6.1/Program.cs
+++ 6.1/Program.cs
@@ -1,74 +1,77 @@
 ﻿using System;
-
-enum HeirColor
+class Sad
 {
-    Red,
-    Green,
-    Blue,
-    Grey,
-    Yellow,
-    Black
-}
-
-class Student
-{
-    public string firstname;
-    public string lastname;
-    public string middlename;
-    public int age;
-    public string birthday;
-    public string sex;
-    public string english_level;
-    public string group;
-    public HeirColor color;
-    public HeirColor body;
-
-
-    public void Funcshn()
+    enum HeirColor
     {
-        Console.WriteLine($"{firstname} {lastname} пропустив пару.");
-        Console.WriteLine($"{firstname} {lastname} вивчає матеріал за одну ніч.");
-        Console.WriteLine($"{firstname} {lastname} йде в армію після нездачі сесії.");
+        Red,
+        Green,
+        Blue,
+        Grey,
+        Yellow,
+        Black
     }
 
-
-    class Program
+    class Student
     {
-        static Student GetStudent()
-        {
-            Student student = new Student();
-            student.firstname = "Georgiy";
-            student.middlename = "Victorovich";
-            student.lastname = "Shevchenko";
-            student.age = 25;
-            student.birthday = "19.03.2000";
-            student.sex = "male";
-            student.color = HeirColor.Grey;
-            student.body = HeirColor.Black;
-            student.english_level = "B2";
-            student.group = "3-B";
-            return student;
-        }
-        static public void Info(Student student)
-        {
+        public string firstname;
+        public string lastname;
+        public string middlename;
+        public int age;
+        public string birthday;
+        public string sex;
+        public string english_level;
+        public string group;
+        public HeirColor color;
+        public HeirColor body;
 
-            Console.WriteLine($"Ім'я: {student.firstname} {student.middlename} {student.lastname}");
-            Console.WriteLine($"Вік: {student.age}");
-            Console.WriteLine($"День народження: {student.birthday}");
-            Console.WriteLine($"Група: {student.group}");
-            Console.WriteLine($"Рівень англійської: {student.english_level}");
-            Console.WriteLine($"Колір волосся: {student.color}");
-            Console.WriteLine($"Колір тіла: {student.body}");
 
+        public void Funcshn()
+        {
+            Console.WriteLine($"{firstname} {lastname} пропустив пару.");
+            Console.WriteLine($"{firstname} {lastname} вивчає матеріал за одну ніч.");
+            Console.WriteLine($"{firstname} {lastname} йде в армію після нездачі сесії.");
         }
 
 
 
-        static void Main(string[] args)
-        {
-            Student student = GetStudent();
-            Info(student);
-            student.Funcshn();
+
+            static Student GetStudent()
+            {
+                Student student = new Student();
+                student.firstname = "Georgiy";
+                student.middlename = "Victorovich";
+                student.lastname = "Shevchenko";
+                student.age = 25;
+                student.birthday = "19.03.2000";
+                student.sex = "male";
+                student.color = HeirColor.Grey;
+                student.body = HeirColor.Black;
+                student.english_level = "B2";
+                student.group = "3-B";
+                return student;
+            }
+            static public void Info(Student student)
+            {
+
+                Console.WriteLine($"Ім'я: {student.firstname} {student.middlename} {student.lastname}");
+                Console.WriteLine($"Вік: {student.age}");
+                Console.WriteLine($"День народження: {student.birthday}");
+                Console.WriteLine($"Група: {student.group}");
+                Console.WriteLine($"Рівень англійської: {student.english_level}");
+                Console.WriteLine($"Колір волосся: {student.color}");
+                Console.WriteLine($"Колір тіла: {student.body}");
+                Console.WriteLine("||||||||||||||||||||||");
+
+            }
+
+
+
+            static void Main(string[] args)
+            {
+                Student student = GetStudent();
+                Info(student);
+                student.Funcshn();
+            }
         }
     }
-}
+
